d512cb06868a6bbbda6692858cd864b16fe2bd0a
Rajeev Toleti <rajeevtoleti2002@gmail.com>
2022-08-25 10:05:41 +0530

    added changes to newUserRoute

--- routes/newUser.js
+++ routes/newUser.js
@@ -1,18 +1,16 @@
-const router = require('express').Router();
-const {User} = require('../models/user');
+const router = require("express").Router();
+const { User } = require("../models/user");
 
-router.post("/", async (req,res) => {
+router.get("/", async (req, res) => {
+  try {
+    const newUser = new User({});
 
-   try{
-        const newUser = new User({});
+    const data = await newUser.save();
 
-        const data = await newUser.save();
-
-        res.status(200).json(data._id);
-   }
-   catch(err){
-      res.status(404).send("Error occured while creating new user.")
-   }
+    res.status(200).json(data._id);
+  } catch (err) {
+    res.status(404).send("Error occured while creating new user.");
+  }
 });
 
 module.exports = router;
